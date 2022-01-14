4e5934bb8265ddaf5a408a281529710e6394aba2
Eder Santos <3derms@gmail.com>
2022-01-14 10:12:53 -0300

    Altera migrations e models

--- models/Category.js
+++ models/Category.js
@@ -1,15 +1,10 @@
-module.exports = (sequelize, DataTypes) => {
-    const Category = sequelize.define('Category', {
+const Category = (sequelize, DataTypes) => {
+  const category = sequelize.define('Category', {
       name: DataTypes.STRING,
-    },
-    {
-      tableName: 'Categories',
+  }, {
       timestamps: false,
-    });
-    Category.associate = (models) => {
-      Category.belongsToMany(models.BlogPost, { 
-        through: models.PostCategory, foreignKey: 'categoryId', as: 'posts',
-      });
-    };
-    return Category;
-  };
+  });
+  return category;
+};
+
+module.exports = Category;
--- models/PostCategory.js
+++ models/PostCategory.js
@@ -1,31 +1,21 @@
-module.exports = (sequelize, _DataTypes) => {
-  const PostCategory = sequelize.define('PostCategory',
-  {},
-  {
-    tableName: 'PostCategories',
-    timestamps: false,
-  });
-  return PostCategory;
+const PostsCategory = (sequelize, _DataTypes) => {
+  const postsCategory = sequelize.define('PostsCategory', {},
+    { timestamps: false });
+  postsCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: postsCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: postsCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+  return postsCategory;
 };
 
-// const PostCategory = (sequelize, _DataTypes) => {
-//   const postCategory = sequelize.define('PostCategory', {},
-//     { timestamps: false });
-//   postCategory.associate = (models) => {
-//     models.BlogPost.belongsToMany(models.Category, {
-//       as: 'categories',
-//       through: postCategory,
-//       foreignKey: 'postId',
-//       otherKey: 'categoryId',
-//     });
-//     models.Category.belongsToMany(models.BlogPost, {
-//       as: 'posts',
-//       through: postCategory,
-//       foreignKey: 'categoryId',
-//       otherKey: 'postId',
-//     });
-//   };
-//   return postCategory;
-// };
-
-// module.exports = PostCategory;
+module.exports = PostsCategory;
